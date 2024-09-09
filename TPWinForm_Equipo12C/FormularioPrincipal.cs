3fa0859d963dd4149056b816d6dd70a3dc01fc35
alemon32 <alejandro32.monzon@gmail.com>
2024-09-09 00:53:18 -0300

    creacion de los form a utlizar, falta creacion de enventos

--- TPWinForm_Equipo12C/FormularioPrincipal.cs
+++ TPWinForm_Equipo12C/FormularioPrincipal.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TPWinForm_Equipo12C
 {
-    public partial class Form1 : Form
+    public partial class FormularioPrincipal : Form
     {
-        public Form1()
+        public FormularioPrincipal()
         {
             InitializeComponent();
         }
--- TPWinForm_Equipo12C/Program.cs
+++ TPWinForm_Equipo12C/Program.cs
@@ -16,7 +16,7 @@ namespace TPWinForm_Equipo12C
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormularioPrincipal());
         }
     }
 }
